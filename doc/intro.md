682aa626e2dc34f72ab02031564489fe35f17c80
Matt Bargar <mbargar@gmail.com>
2014-02-28 11:23:25 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to crello
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/crello/core_test.clj
@@ -0,0 +1,7 @@
+(ns crello.core-test
+  (:require [clojure.test :refer :all]
+            [crello.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
