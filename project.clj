b6fee940226903a17eae9b21a5f15e66ee21cff4
mbargar <mbargar@gmail.com>
2014-03-03 22:13:35 -0500

    update metadata and core namespace def

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject crello "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Experiments with Clojure and Trello"
+  :url "https://github.com/Bargs/crello"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
--- src/crello/core.clj
+++ src/crello/core.clj
@@ -1,5 +1,10 @@
 (ns crello.core
+  (:require [clj-http.client :as client])
   (:gen-class))
+
+(defn testGet [url]
+  (:body (client/get url {:as :json})))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
