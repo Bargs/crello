b506eb2d8fbb7f6db2c33e592d7d16c34ef92b41
mbargar <mbargar@gmail.com>
2014-03-03 22:49:25 -0500

    pulling in API key from resource file and and playing around with gets

--- src/crello/core.clj
+++ src/crello/core.clj
@@ -1,10 +1,15 @@
 (ns crello.core
   (:require [clj-http.client :as client])
+  (:require [clojure.edn :as edn])
+  (:require [clojure.java.io :as jio])
   (:gen-class))
 
-(defn testGet [url]
-  (:body (client/get url {:as :json})))
+(def secrets (edn/read-string (slurp (jio/resource "secrets.edn"))))
 
+(defn testGet [url]
+  (:body (client/get (str url "?key=" (:trelloAppKey secrets)) {:as :json})))
+
+(testGet "https://api.trello.com/1/boards/4d5ea62fd76aa1136000000c")
 
 (defn -main
   "I don't do a whole lot ... yet."
